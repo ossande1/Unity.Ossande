661133107f08278ba37e9876a7c141695c2e0688
Ossande Gima Da silva <9030460@student.zadkine.nl>
2026-03-24 11:30:12 +0100

    Add files via upload

---
+++ Dropper.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    [SerializeField] float timeToWait = 2f;
+
+    MeshRenderer myMeshRenderer;
+    Rigidbody myRigidbody;
+   
+    void Start()
+    {
+        myMeshRenderer = GetComponent<MeshRenderer>();
+        myRigidbody = GetComponent<Rigidbody>();
+        myMeshRenderer.enabled = false;
+        myRigidbody.useGravity = false;
+    }
+
+    void Update()
+    {
+        if (Time.time > timeToWait)
+        {
+            Debug.Log("Lookout below!");
+            myMeshRenderer.enabled = true;
+            myRigidbody.useGravity = true;
+        }
+    }
+}
