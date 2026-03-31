db5b8ebc8869bb07f868338a02f2646e2df2a382
Ossande Gima Da silva <9030460@student.zadkine.nl>
2026-03-31 11:12:26 +0200

    Add files via upload

---
+++ scripts/Movement.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStregth = 100f;
+    [SerializeField] float rotationStrength = 100f;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+         rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        thrust.Enable();
+        rotation.Enable();
+    }
+
+    void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStregth * Time.fixedDeltaTime);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if(rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if(rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+    }
+}
